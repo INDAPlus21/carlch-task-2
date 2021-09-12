a12c03e8c07589ec66426bf18d408e9a21d865d2
Chemnitz <carlch@kth.se>
2021-09-12 19:28:58 +0200

    Add Summera_Tal folder and solution

---
+++ SummeraTal/target/debug/SummeraTal.d
@@ -0,0 +1,1 @@
+/Users/chemnitz/Repos/carlch-task-2/SummeraTal/target/debug/SummeraTal: /Users/chemnitz/Repos/carlch-task-2/SummeraTal/src/main.rs
---
+++ SummeraTal/target/debug/deps/SummeraTal.d
@@ -0,0 +1,5 @@
+/Users/chemnitz/Repos/carlch-task-2/SummeraTal/target/debug/deps/SummeraTal: src/main.rs
+
+/Users/chemnitz/Repos/carlch-task-2/SummeraTal/target/debug/deps/SummeraTal.d: src/main.rs
+
+src/main.rs:
